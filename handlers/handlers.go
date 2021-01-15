1cfb6004f07dcf5d4f42c5a13c211bb6c43d1cb7
juan pablo <glup87@yahoo.com.ar>
2021-01-15 13:05:50 -0300

    modified:   handlers/handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,11 +7,17 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"github.com/zeeno77/microblog/middlew"
+	"github.com/zeeno77/microblog/routers"
 )
 
 /*Manejadores: seteo mi puerto, el handler y pongo a escuchar al servidor*/
 func Manejadores() {
 	router := mux.NewRouter()
+
+	/*Por cada EndPoint vamos a tener un renglón de código que permita manejar la función correspondiente*/
+	router.HandleFunc("/registro", middlew.ChequeoDB(routers.Registro)).Methods("POST")
+
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "8080"
